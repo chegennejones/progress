2caa55fbb1ecdab5dfa401e3cdd44cf0d447a5cf
cheyenne jones <chey@macbook-pro.local>
2021-07-01 20:23:00 -0500

    controller

--- progress/progress/ViewController.swift
+++ progress/progress/ViewController.swift
@@ -19,22 +19,54 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+ 
     @IBAction func teethbutton(_ sender: Any) {
+        let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: "brush teeth")
+                attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
+                
+                
+        teethdisplay.attributedText = attributeString;
     }
     
     @IBAction func facebutton(_ sender: Any) {
+        let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: "wash face")
+                attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
+                
+                
+        facedisplay.attributedText = attributeString;
     }
     
     @IBAction func showerbutton(_ sender: Any) {
-    }
-
-    @IBAction func foodbutton(_ sender: Any) {
+        let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: "take shower")
+                attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
+                
+                
+        showerdisplay.attributedText = attributeString;
     }
     
+    @IBAction func foodbutton(_ sender: Any) {
+        let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: "eat food")
+                attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
+                
+                
+        fooddisplay.attributedText = attributeString;
+    }
+    
+    
     @IBAction func waterbutton(_ sender: Any) {
+        let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: "drink water")
+                attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
+                
+                
+        waterdisplay.attributedText = attributeString;
     }
     
     @IBAction func sleepbutton(_ sender: Any) {
+        let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: "sleep")
+                attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
+                
+                
+        sleepdisplay.attributedText = attributeString;
     }
     
 }
