bfb4836c4338e256dbd010b55fe7e52ac9202135
cheyenne jones <chey@macbook-pro.local>
2021-07-01 19:23:10 -0500

    controller

--- progress/progress/ViewController.swift
+++ progress/progress/ViewController.swift
@@ -8,13 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var teethdisplay: UILabel!
+    @IBOutlet weak var facedisplay: UILabel!
+    @IBOutlet weak var showerdisplay: UILabel!
+    @IBOutlet weak var fooddisplay: UILabel!
+    @IBOutlet weak var waterdisplay: UILabel!
+    @IBOutlet weak var sleepdisplay: UILabel!
     
-
         override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func teethbutton(_ sender: Any) {
     }
     
+    @IBAction func facebutton(_ sender: Any) {
+    }
+    
+    @IBAction func showerbutton(_ sender: Any) {
+    }
 
+    @IBAction func foodbutton(_ sender: Any) {
+    }
+    
+    @IBAction func waterbutton(_ sender: Any) {
+    }
+    
+    @IBAction func sleepbutton(_ sender: Any) {
+    }
+    
+}
